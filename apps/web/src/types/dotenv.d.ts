ad0338edb3543beb2b175ff8e6e44117018e985b
mdirshaddev <mdirshaddev+github@gmail.com>
2023-10-14 21:23:39 +0530

    node(typings): 🔐 typings for environment secrets

---
+++ apps/web/src/types/dotenv.d.ts
@@ -0,0 +1,9 @@
+declare module NodeJS {
+  interface ProcessEnv {
+    // Google Analytics
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: string;
+
+    // Microsoft Clarity
+    NEXT_PUBLIC_MICROSOFT_CLARITY: string;
+  }
+}

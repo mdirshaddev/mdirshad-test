f574c00e05579b46c7af1cf1ef2fe68bea23efad
mdirshaddev <mdirshaddev+github@gmail.com>
2023-10-16 09:09:38 +0530

    export(hooks): 🧑‍🚀 ready to use hook useWindowSize

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,4 +1,6 @@
 // Exports of all hooks
 export { default as useWindowEvent } from "./use-window-event";
+export { default as useWindowSize } from "./use-window-size";
 
 export * from "./use-window-event";
+export * from "./use-window-size";

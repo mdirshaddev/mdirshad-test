fe5e4f30dccca93a3792d4e0a82418c5064608b1
mdirshaddev <mdirshaddev+github@gmail.com>
2023-10-28 22:12:39 +0530

    next(app): 💻 trpc api route

---
+++ apps/web/src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,13 @@
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+import { appRouter } from "src/modules/server";
+
+const handler = (req: Request) =>
+  fetchRequestHandler({
+    createContext: () => ({}),
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+  });
+
+export { handler as GET, handler as POST };

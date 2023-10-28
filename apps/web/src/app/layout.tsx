18a339fe143c3f03f5b109e56e2a39ccaa86daa3
mdirshaddev <mdirshaddev+github@gmail.com>
2023-10-28 20:56:45 +0530

    next(app): 💻 formatted root layout file

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -41,24 +41,20 @@ export default function RootLayout(props: React.PropsWithChildren) {
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          gtag("js", new Date());
+          gtag("config", "${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}", {
             page_path: window.location.pathname,
           });
         `}
         </Script>
         {/* Microsoft Clarity */}
-        <Script
-          type="text/javascript"
-          strategy="afterInteractive"
-          id="behaviour-analytics"
-        >
+        <Script strategy="afterInteractive" id="behaviour-analytics">
           {`
         (function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY});
+        })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
           `}
         </Script>
         <WebVitals />

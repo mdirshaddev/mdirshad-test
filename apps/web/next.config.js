388248f223c7d81080eea69ebe01925f6ace075e
mdirshaddev <mdirshaddev+github@gmail.com>
2023-11-23 08:50:22 +0530

    prettier(format): 🏗️ applied prettier formatting

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,54 +2,54 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: false
   },
   experimental: {
-    webVitalsAttribution: ["CLS", "FCP", "FID", "INP", "LCP", "TTFB"],
-    serverComponentsExternalPackages: ["@prisma/client"],
+    webVitalsAttribution: ['CLS', 'FCP', 'FID', 'INP', 'LCP', 'TTFB'],
+    serverComponentsExternalPackages: ['@prisma/client']
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com", // GitHub Avatar
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com' // GitHub Avatar
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com", // Cloudinary
+        protocol: 'https',
+        hostname: 'res.cloudinary.com' // Cloudinary
       },
       {
-        protocol: "https",
-        hostname: "i.scdn.co", // Spotify Album
+        protocol: 'https',
+        hostname: 'i.scdn.co' // Spotify Album
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com", // Unsplash Images
-      },
-    ],
+        protocol: 'https',
+        hostname: 'images.unsplash.com' // Unsplash Images
+      }
+    ]
   },
   modularizeImports: {
     lodash: {
-      transform: "lodash/{{member}}",
+      transform: 'lodash/{{member}}'
     },
-    "date-fns": {
-      transform: "date-fns/{{member}}",
+    'date-fns': {
+      transform: 'date-fns/{{member}}'
     },
-    "react-icons": {
-      transform: "react-icons/{{member}}",
-    },
+    'react-icons': {
+      transform: 'react-icons/{{member}}'
+    }
   },
   swcMinify: true,
   typescript: {
-    ignoreBuildErrors: false,
-  },
+    ignoreBuildErrors: false
+  }
 };
 
-if (process.env.IS_VERCEL == "false") {
-  console.log("Docker build for Next.js application image");
-  nextConfig["output"] = "standalone"; // for docker it is required
+if (process.env.IS_VERCEL == 'false') {
+  console.log('Docker build for Next.js application image');
+  nextConfig['output'] = 'standalone'; // for docker it is required
   module.exports = nextConfig;
 } else {
-  console.log("Vercel or Locally build for Next.js deployment or development");
+  console.log('Vercel or Locally build for Next.js deployment or development');
   module.exports = nextConfig;
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,96 +1,96 @@
-import plugin from "tailwindcss/plugin";
+import plugin from 'tailwindcss/plugin';
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["src/**/*.{ts,tsx}"],
-  darkMode: ["class"],
+  content: ['src/**/*.{ts,tsx}'],
+  darkMode: ['class'],
   plugins: [
     plugin(function ({ addVariant }) {
-      addVariant("hocus", ["&:hover", "&:focus"]);
+      addVariant('hocus', ['&:hover', '&:focus']);
     }),
-    require("tailwindcss-animate"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
+    require('tailwindcss-animate'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
   ],
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: '2rem',
       screens: {
-        "2xl": "1400px",
-      },
+        '2xl': '1400px'
+      }
     },
     extend: {
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
       },
       backgroundImage: {
-        "hero-right-side":
-          "radial-gradient(farthest-corner circle at 0% 0%, rgba(0, 58, 117, 0.3) 0%, #101418 100%)",
-        "navigation-blog":
-          "linear-gradient(-225deg, #65379B 0%, #886AEA 53%, #6457C6 100%)",
-        "navigation-insights":
-          "linear-gradient(-225deg, #77FFD2 0%, #6297DB 48%, #1EECFF 100%)",
-        "navigation-works":
-          "linear-gradient(-225deg, #7742B2 0%, #F180FF 52%, #FD8BD9 100%)",
-        "showcase-container":
-          "linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%)",
+        'hero-right-side':
+          'radial-gradient(farthest-corner circle at 0% 0%, rgba(0, 58, 117, 0.3) 0%, #101418 100%)',
+        'navigation-blog':
+          'linear-gradient(-225deg, #65379B 0%, #886AEA 53%, #6457C6 100%)',
+        'navigation-insights':
+          'linear-gradient(-225deg, #77FFD2 0%, #6297DB 48%, #1EECFF 100%)',
+        'navigation-works':
+          'linear-gradient(-225deg, #7742B2 0%, #F180FF 52%, #FD8BD9 100%)',
+        'showcase-container':
+          'linear-gradient(-225deg, #69EACB 0%, #EACCF8 48%, #6654F1 100%)'
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
       },
       colors: {
         accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))'
         },
-        background: "hsl(var(--background))",
-        border: "hsl(var(--border))",
+        background: 'hsl(var(--background))',
+        border: 'hsl(var(--border))',
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))'
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))'
         },
-        foreground: "hsl(var(--foreground))",
-        input: "hsl(var(--input))",
+        foreground: 'hsl(var(--foreground))',
+        input: 'hsl(var(--input))',
         muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))'
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))'
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))'
         },
-        ring: "hsl(var(--ring))",
+        ring: 'hsl(var(--ring))',
         secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))'
+        }
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
       },
       screens: {
-        "touch-device": { raw: "(hover: none)" },
-        "touch-no-animation": { raw: "(prefers-reduced-motion)" },
-      },
-    },
-  },
+        'touch-device': { raw: '(hover: none)' },
+        'touch-no-animation': { raw: '(prefers-reduced-motion)' }
+      }
+    }
+  }
 };
